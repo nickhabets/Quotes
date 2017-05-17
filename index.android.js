606e5fb8dd6329790f0b476726449bc39a158efa
Sasa <sasa.simovic931ca@gmail.com>
2017-05-17 09:48:36 +0200

    Android app

--- index.android.js
+++ index.android.js
@@ -1,53 +1,25 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * Quotes React Native App
+ * https://github.com/sasasim/Quotes
  * @flow
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux'
 
-export default class Quotes extends Component {
+import store from './src/store'
+
+import App from './src/app'
+
+export default class AppContainer extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Provider store={store}>
+        <App/>
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('Quotes', () => Quotes);
+AppRegistry.registerComponent('Quotes', () => AppContainer);
--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,6 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * Quotes React Native App
+ * https://github.com/sasasim/Quotes
  * @flow
  */
 
